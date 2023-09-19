7a1d54bed01e602e32f7e1c659fe144358da1f20
ROSHNI PRASAD <84218269+roshniprasad01@users.noreply.github.com>
2023-09-19 11:32:26 +0530

    Add files via upload

--- mbs/src/components/movieDes.js
+++ mbs/src/components/movieDes.js
@@ -1,29 +1,48 @@
-import React from 'react';
+
+import React, { useState, useEffect } from 'react';
 import './movieDes.css';
 
-const movieDes = () => {
-  // Replace these with your movie details
-  const movie = {
+const MovieDes = ({ movieId }) => {
+  const [movie, setMovie] = useState({
     name: 'RDX',
-    rating: 8.5/10,
-    description: 'Robert Dony Xavier is a revenge drama dealing in more than two timelines.',
-    language: 'English',
+    rating: 8.5,
+    description: '',
+    language: 'Malayalam',
+    imageUrl: '',
+  });
+
+  useEffect(() => {
+    // Replace 'your_backend_api_url' with your actual API endpoint that accepts a movie_id parameter
+    fetch(`your_backend_api_url?movie_id=${movieId}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setMovie(data); // Update movie state with data from the backend
+      })
+      .catch((error) => {
+        console.error('Error fetching movie data:', error);
+      });
+  }, [movieId]); // Trigger the effect whenever movieId changes
+  const aboutStyle={
+    color: 'White',
   };
-  
   return (
     <div className="c1">
       <div className="movie-details">
         <h2>{movie.name}</h2>
-        <p>Rating: {movie.rating}/10</p>
-        <p>Description: {movie.description}</p>
-        <p>Language: {movie.language}</p>
-        <button className='b1'>Book tickets</button>
+        <img src={movie.imageUrl} alt={movie.name} className="movie-poster" />
+        <div className='content'>
+          <p>Rating: {movie.rating}/10</p>
+          <p>Language: {movie.language}</p>
+          <button className='b1'>Book tickets</button>
+        </div>
       </div>
+      <div className='about' style={aboutStyle}>
       <h3>About The Movie</h3>
-      {movie.description}    
+      <p>{movie.description}</p>
+      </div>
     </div>
   );
 };
 
-export default movieDes;
+export default MovieDes;
 
