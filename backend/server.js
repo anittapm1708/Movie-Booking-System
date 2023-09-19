b1b09681c0041a82614542e2912f63261e1b4e3c
ashnapk23 <ashnapk23@gmail.com>
2023-09-19 09:40:17 +0530

    backendLogin

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,9 @@ const cors=require('cors');
 const mongoose=require('mongoose');
 app.use(cors());
 app.use(express.json());
-
+const {registerCreate} = require('./controllers/registerController')
 const {BookedSeat}=require('./controllers/SeatsController');
+const {Login}=require('./controllers/loginController')
 
 const MONGODB_URL="mongodb+srv://anittapm1708:anittapm@cluster0.ru7auzd.mongodb.net/Movie_Booking?retryWrites=true&w=majority";
 mongoose.connect(MONGODB_URL).then(()=>{
@@ -17,7 +18,8 @@ mongoose.connect(MONGODB_URL).then(()=>{
 });
 
 app.get("/book",BookedSeat);
-
+app.post("/register",registerCreate)
+app.post("/login",Login)
 app.listen(port,()=>{
 console.log("server running on port :",port);
 })
