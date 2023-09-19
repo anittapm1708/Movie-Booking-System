049a89468af21c1bb528e84e844bf7c7ac172602
NAVITHA16 <107757366+NAVITHA16@users.noreply.github.com>
2023-09-19 11:44:15 +0530

    Add files via upload

--- mbs/src/components/Page.js
+++ mbs/src/components/Page.js
@@ -2,78 +2,29 @@
 
 import React from 'react';
 import './Page.css';
+import Rectangle from './Rectangle'; // Import the Rectangle component
 
 function Page() {
   return (
     <div className="page">
-         <div className="main_rectangle">
-         <span className="arrow">&lt;</span>
-        <div className="square">
-          <span className='md'>19 SEP</span>
+      <div className="main_rectangle">
+        <span className="arrow">&lt;</span>
+        <div className="square1">
+          <span className='md1'>19 SEP</span>
         </div>
-        <div className="square">
-          <span className='md'>20 SEP</span>
+        <div className="square2">
+          <span className='md2'>20 SEP</span>
         </div>
-        <div className="square">
-          <span className='md'>21 SEP</span>
-           {/* <span className="arrow">&gt;</span> */}
+        <div className="square3">
+          <span className='md3'>21 SEP</span>
+          {/* <span className="arrow">&gt;</span> */}
         </div>
         <span className="arrow">&gt;</span>
       </div>
-      <div className="rectangle">
-        <div className="text-left">Jawan(UA)</div>
-        <div className="center-right-container">
-          <div className="center-rectangle">
-            <hp>10:00 am</hp>
-          </div>
-          <div className="center-rectangle">
-          <hp>12:00 pm</hp>
-          </div>
-          <div className="center-rectangle">
-          <hp>4:00 pm</hp>
-          </div>
-          <div className="center-rectangle">
-          <hp>10:00 pm</hp>
-          </div>
-        </div>
-        {/* <div className="text-right">Text Right 1</div> */}
-      </div>
-      <div className="rectangle">
-        <div className="text-left">RDX(UA)</div>
-        <div className="center-right-container">
-          <div className="center-rectangle">
-          <hp>10:00 am</hp>
-          </div>
-          <div className="center-rectangle">
-          <hp>11:00 am</hp>
-          </div>
-          <div className="center-rectangle">
-          <hp>1:00 pm</hp>
-          </div>
-          <div className="center-rectangle">
-          <hp>10:00 pm</hp>
-          </div>
-        </div>
-        {/* <div className="text-right"></div> */}
-      </div>
-      <div className="rectangle">
-        <div className="text-left">Nun 2(UA)</div>
-        <div className="center-right-container">
-          <div className="center-rectangle">
-          <hp>10:00 am</hp>
-          </div>
-          <div className="center-rectangle">
-          <hp>12:00 pm</hp>
-          </div>
-          <div className="center-rectangle">
-          <hp>5:00 pm</hp>
-          </div>
-          <div className="center-rectangle">
-          <hp>10:00 pm</hp>
-          </div>
-        </div>
-        {/* <div className="text-right">Text Right 3</div> */}
-      </div>
+      
+      {/* Include the Rectangle component */}
+      <Rectangle />
+      
     </div>
   );
 }
---
+++ mbs/src/components/Rectangle.js
@@ -0,0 +1,43 @@
+// Rectangle.js
+
+import React from 'react';
+import './Page.css';
+function Rectangle({ movie_id }) {
+  // Define a mapping of movie_ids to corresponding text
+  const movieTextMap = {
+    1: 'Jawan(UA)',
+    2: 'RDX(UA)',
+    3:'Nun(UA)',
+    // Add more movie_ids and corresponding text as needed
+  };
+
+  // Get the text based on the movie_id or provide a default value
+  const movieText = movieTextMap[movie_id] || 'Unknown Movie';
+
+  return (
+    <div className="rectangle">
+      <div className="text-left">{movieText}</div>
+      <div className="center-right-container">
+        <div className="center-rectangle">
+          <div>
+          <pm>10:00 am  </pm></div>
+          <pt>DOLBY 7.1</pt>
+        </div>
+        <div className="center-rectangle">
+          <div><pm>12:00 pm</pm></div>
+          <pt>DOLBY 7.1</pt>   
+        </div>
+        <div className="center-rectangle">
+          <div><pm>4:00 pm</pm></div>
+          <pt>DOLBY 7.1</pt>
+        </div>
+        <div className="center-rectangle">
+          <div><pm>10:00 pm</pm></div>
+          <pt>DOLBY 7.1</pt>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Rectangle;
